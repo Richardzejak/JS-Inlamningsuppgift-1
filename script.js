13ea9395f101650c035420ae26d9212d436b0ae7
Richard Zejak <richardzejak@gmail.com>
2021-01-21 14:14:30 +0000

    working variables and contruction for handling the counting of textboxes

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-let elementCount;
+let elementCount =0;
+let elementChange =0;
+
 
 document.getElementById("blogamount").addEventListener('change', setEqual);
-
 function setEqual(){
-  elementCount=blogamount.value;
-  alert(elementCount);
+  if (blogamount.value>20)
+  {
+  blogamount.value="20";
+  alert("Cant Set more than 20 Blog Inputs!");
+  }
+  if (blogamount.value < 0)
+  {
+  blogamount.value="0";
+  alert("Cant set negative number")
+  }
+  elementChange=blogamount.value-elementCount; // ger skillnadens värde till elementChange
+  elementCount=blogamount.value; // sätter elementcount till blogamounts värde
+  while (elementChange>0)
+  {
+    createBlogInput(); // kallar på att skapa objekt funktionerna
+    elementChange--;
+  }
+  while(elementChange<0)
+  {
+    removeBlogInput();
+    elementChange++;
+  }
 }
 
+function createBlogInput(){
+  let blogInput = document.createElement("input");
+  blogInput.type="text";
+  blogInput.className = "blogInputClass";
+  blogInput.id = "box";
+  var parent = document.getElementById("bloginputcontainer");
+  parent.appendChild(blogInput);
+}
+
+function removeBlogInput(){
+var lastCreatedElement = document.getElementById("box");
+lastCreatedElement.remove();
+}
 
 /* function addElement () {
    const newDiv = document.createElement("div");
@@ -17,5 +51,5 @@ function setEqual(){
    const currentDiv = document.getElementById("div1");
    document.body.insertBefore(newDiv, currentDiv);
 
-   elementCount++; */
- }
+   elementCount++;
+ }*/
