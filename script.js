28af5c6185dac9427432399e47227e913b08c6d8
Richard Zejak <richardzejak@gmail.com>
2021-01-21 10:29:45 +0000

    Created fully working variable with eventlistener that checks current input of the input

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
- 
+let elementCount;
+
+document.getElementById("blogamount").addEventListener('change', setEqual);
+
+function setEqual(){
+  elementCount=blogamount.value;
+  alert(elementCount);
+}
+
+
+/* function addElement () {
+   const newDiv = document.createElement("div");
+   const newContent = document.createTextNode("Hey hey");
+
+   newDiv.appendChild(newContent);
+
+   const currentDiv = document.getElementById("div1");
+   document.body.insertBefore(newDiv, currentDiv);
+
+   elementCount++; */
+ }
